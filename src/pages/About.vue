334345f87436aa2f801eb6a922e55ad9db280fea
Zachary Guerrero <zack.grrr@gmail.com>
2020-09-08 19:17:18 -0700

    add images for demo

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   Layout
-    .container(class="mx-auto")
-      h1 About Us
-      p Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea dolor eius repudiandae tenetur magni repellat minus saepe. Accusantium exercitationem, dolorum porro magnam voluptatem inventore consectetur nihil provident blanditiis, hic beatae.
+    .container(class="px-4 mx-auto mt-16")
+      h1(class="font-bold text-4xl") About Us
+      p(class="text-xl mt-4 text-gray-700 leading-loose") Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea dolor eius repudiandae tenetur magni repellat minus saepe. Accusantium exercitationem, dolorum porro magnam voluptatem inventore consectetur nihil provident blanditiis, hic beatae.
   
 </template>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,45 @@
 <template lang="pug">
 
   Layout
-    .container(class="mx-auto")
-      //- Learn how to use images here: https://gridsome.org/docs/images
-      g-image(alt="example image" src="~/favicon.png" width="135")
+    .homepage-hero(class="bg-blue-200")
 
-      h1 Hello, world!
+      .hero-container(class="grid grid-cols-2 md:grid-cols-4")
+        .gridsome(class="h-20 bg-gridsome flex items-center justify-center py-32 md:py-48")
+          g-image(src="~/assets/gridsome-dark.svg" class="h-16")
+        .pug(class="h-20 bg-pug flex items-center justify-center py-32 md:py-48")
+          g-image(src="~/assets/pug.svg" class="h-24")
+        .stylus(class="h-20 bg-stylus flex items-center justify-center py-32 md:py-48")
+          g-image(src="~/assets/stylus.svg" class="h-16")
+        .tailwind(class="h-20 bg-tailwind flex items-center justify-center py-32 md:py-48")
+          g-image(src="~/assets/tailwind.svg" class="h-16")
 
-      p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    .container(class="max-w-2xl px-4 mx-auto mt-16 text-center")
 
-      p.home-links
-        a(href="https://gridsome.org/docs/" target="_blank" rel="noopener") Gridsome Docs
-        a(href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener") GitHub
-    
+      h1(class="font-bold text-4xl leading-tight") The 
+        span(class="italic") Easiest 
+        | way to develop on Gridsome!
+
+      p(class="text-xl mt-8 leading-loose text-gray-700") As if developing on Gridsome wasn't easy enough already, When you mix Pug, Stylus, and TailwindCSS into the mix development gets taken to the next level!
+
+      
+      
 </template>
   
 <style lang="stylus">
+  $gridsome-green = #00a672
+  $pug-red = #a86454
+  $tailwind-cyan = #edf2f7
+  $stylus-orange = #ff6347
+
+  .bg-gridsome
+    background-color $gridsome-green
+  .bg-pug
+    background-color $pug-red
+  .bg-tailwind
+    background-color $tailwind-cyan
+  .bg-stylus
+    background-color $stylus-orange
+
 </style>
 
 <script>
